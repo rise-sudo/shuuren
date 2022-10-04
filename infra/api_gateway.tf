cf8da13e18677f3b06862c0221780d1d4e8ac044
rise-sudo <syedur.app@gmail.com>
2022-10-04 19:56:08 -0400

    fix: resource typo

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "shuuren_api_gateway_rest_api" {
 }
 
 resource "aws_api_gateway_resource" "shuuren_api_gateway_resource" {
-  parent_id   = aws_api_gateway_rest_api.shuuren_api_gateway.root_resource_id
+  parent_id   = aws_api_gateway_rest_api.shuuren_api_gateway_rest_api.root_resource_id
   path_part   = "api"
-  rest_api_id = aws_api_gateway_rest_api.shuuren_api_gateway.id
+  rest_api_id = aws_api_gateway_rest_api.shuuren_api_gateway_rest_api.id
 }
