3540f344651eb2bbb8e9fceb1a43affc0d50c3f8
Syedur <80500405+rise-sudo@users.noreply.github.com>
2022-10-05 19:34:57 -0400

    feat: set up cognito user pool (#20)

---
+++ infra/cognito.tf
@@ -0,0 +1,20 @@
+resource "aws_cognito_user_pool" "shuuren_user_pool" {
+  name = "shuuren_user_pool"
+}
+
+resource "aws_cognito_user_pool_client" "shuuren_client" {
+  name                                 = "shuuren_client"
+  user_pool_id                         = aws_cognito_user_pool.shuuren_user_pool.id
+  callback_urls                        = ["https://example.com/callback"]
+  logout_urls                          = ["https://example.com/signout"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid"]
+  supported_identity_providers         = ["COGNITO"]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain          = "auth.shuuren.com"
+  certificate_arn = aws_acm_certificate.shuuren_cert.arn
+  user_pool_id    = aws_cognito_user_pool.shuuren_user_pool.id
+}
