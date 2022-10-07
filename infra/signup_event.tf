f57b2aaaa46e2a45b60a6123cd3a9b3dd0713556
rise-sudo <syedur.app@gmail.com>
2022-10-07 00:11:55 -0400

    feat: update role policy to access cloudwatch

--- infra/signup_event.tf
+++ infra/signup_event.tf
@@ -23,6 +23,32 @@ resource "aws_iam_role" "signup_event_role" {
   })
 }
 
+resource "aws_iam_policy" "signup_event_logging" {
+  name        = "signup_event_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = [
+      {
+        "Action" = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" = "arn:aws:logs:*:*:*",
+        "Effect"   = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "signup_event_lambda_logs" {
+  role       = aws_iam_role.signup_event_role.name
+  policy_arn = aws_iam_policy.signup_event_logging.arn
+}
+
 resource "aws_lambda_function" "signup_post_confirmation" {
   filename         = "signup.zip"
   function_name    = "signup_post_confirmation"
