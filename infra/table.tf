a847e3d022283c3f5cf199d701b1c7df2c8295c8
rise-sudo <syedur.app@gmail.com>
2022-10-07 00:29:16 -0400

    feat: create table for user stats

--- infra/table.tf
+++ infra/table.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "stats" {
-  name           = "stats"
+resource "aws_dynamodb_table" "character_info" {
+  name           = "character_info"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
