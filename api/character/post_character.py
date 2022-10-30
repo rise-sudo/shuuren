501631ce078d82706e2b4e31885f06c94e17a9ad
Syedur Rahman <80500405+rise-sudo@users.noreply.github.com>
2022-10-30 18:09:37 -0400

    feat: reset character stats api (#74)

--- api/character/post_character.py
+++ api/character/post_character.py
@@ -4,13 +4,44 @@ import json
 def post_character(query_params, dynamodb):
     """ post character
     the http function that invokes when the basic character 
-    stats are requested to be replaced entirely by the user """
+    stats are requested to be replaced entirely by the user
+    in other words this resets the user stats as well as removes any custom stats """
 
     # initialize response
     response = {'statusCode': '', 'body': ''}
 
-    # WIP - future feat
-    response['statusCode'] = 400
-    response['body'] = json.dumps({'message': 'HTTP Method not available yet'})
+    # select the appropriate table
+    character_table = dynamodb.Table('character')
+
+    # attempt to get the character data
+    character_info = character_table.get_item(
+        Key={
+            'user': query_params['user']
+        }
+    )
+
+    # if the character info is available
+    if character_info.get('Item'):
+        # reset the values entirely with defaults
+        character_table.put_item(
+           Item={
+                'user': query_params['user'],
+                'str': '10',
+                'dex': '10',
+                'int': '10',
+                'level': '1',
+                'exp': '0'
+            }
+        )
+
+        # set the response
+        response['statusCode'] = 200
+        response['body'] = json.dumps({'message': f"User {query_params['user']} stats have been reset successfully"})
+
+    # otherwise assume no such user exists
+    else:
+        # set the response
+        response['statusCode'] = 400
+        response['body'] = json.dumps({'message': 'User does not exist'})
 
     return response
